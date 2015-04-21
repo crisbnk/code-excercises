51fe323667e66633993e69817f3d8812a9db035f
Cristiano <cristiano.bianchi@gmail.com>
2015-04-21 14:06:07 +0200

    Update introduction

--- mutuations-2.js
+++ mutuations-2.js
@@ -1,11 +1,10 @@
-// MUTATIONS solution 2
-// Informations about the excercise in Bonfire:
-// "Return true if the string in the first element of the array
+// MUTATIONS solution 2 - Bonfire exercise
+// Return true if the string in the first element of the array
 // contains the letters of the string in the second element of the array.
 // For example, ['hello', 'Hello'], should return true because all of the letters in the second string
 // are present in the first, ignoring case.
 // The arguments ['hello', 'hey'] should return false because the string 'hello' does not contain a 'y'.
-// Another example, ['Alien', 'line'], should return true because all of the letters in 'line' are present in 'Alien'."
+// Another example, ['Alien', 'line'], should return true because all of the letters in 'line' are present in 'Alien'.
 
 function mutuation(arr) {
   var str0 = arr[0].toLowerCase();
