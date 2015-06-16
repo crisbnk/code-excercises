cc2651b14591faf7a06a5fb9cc11ea6d2df3397b
Cristiano <cristiano.bianchi@gmail.com>
2015-06-17 01:37:55 +0200

    Create boo-who.js

---
+++ boo-who.js
@@ -0,0 +1,15 @@
+// BOO WHO - Bonfire excercise on FreeCodeCamp
+// Check if a value is classified as a boolean primitive. Return true or false.
+// Boolean primitives are true and false.
+
+function boo(bool) {
+  var x = typeof bool,
+      res = null;
+  if (x === "boolean") {
+    return res  = true;
+  } else {
+    return res = false;
+  }
+  
+  return res;
+}
